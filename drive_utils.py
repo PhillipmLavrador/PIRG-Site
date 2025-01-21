7dcabe07380c93eaaeebee1dac373af39a8d971c
Phillip Lavrador <81980581+PhillipmLavrador@users.noreply.github.com>
2025-01-20 20:21:24 -0800

    Add files via upload

---
+++ drive_utils.py
@@ -0,0 +1,26 @@
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import pickle
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def authenticate_drive():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return build('drive', 'v3', credentials=creds)
+
+def search_files(service, query):
+    results = service.files().list(q=f"name contains '{query}'", fields="files(id, name)").execute()
+    return results.get('files', [])
